e81f3f63a0c70d66b1baf152f940f3f9bc7dcb93
Pedro Prieto <pedpral@gmail.com>
2021-05-25 11:44:58 +0200

    Añado comentarios y renombro fichero

---
+++ ejercicios/operaciones.py
@@ -0,0 +1,5 @@
+# En principio el alumno solo recibiría la definición de la función en el archivo correspondiente
+def suma(x,y):
+
+    # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente
+    return x + y
--- tests/test_suma.py
+++ tests/test_suma.py
@@ -1,7 +1,13 @@
+# Librería pytest para ejecutar tests
 import pytest
-from ejercicios.suma import suma
 
+# Importar archivo de ejercicio
+from ejercicios.operaciones import suma
+
+# Clase para crear tests. Las funciones de testeo deberán crearse en esta clase
 class TestClass:
+
+    # Test para la operación suma
     def test_suma(self):
         assert suma(4,5) == 9
         assert suma(-1,-2) == -3
